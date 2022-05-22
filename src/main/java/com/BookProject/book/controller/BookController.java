4c77f3d3fe5235760357faa76f24e67874d769f0
azamat <azamatnematov2003@gmail.com>
2022-05-22 18:56:27 +0500

    update bookController

--- src/main/java/com/BookProject/book/controller/BookController.java
+++ src/main/java/com/BookProject/book/controller/BookController.java
@@ -15,43 +15,44 @@ import java.util.List;
 @AllArgsConstructor
 public class BookController {
 
-    private  final BookService bookService;
+    private final BookService bookService;
 
     @PostMapping("/create")
-    public ResponseEntity<?> create (@RequestBody @Valid BookDto bookDto){
-        boolean result= bookService.create(bookDto);
+    public ResponseEntity<?> create(@RequestBody @Valid BookDto bookDto) {
+        boolean result = bookService.create(bookDto);
         return ResponseEntity.ok(result);
 
     }
 
     @GetMapping("/get/{id}")
-    public ResponseEntity<?> get(@PathVariable("id") Integer id){
-        BookDto result=bookService.get(id);
+    public ResponseEntity<?> get(@PathVariable("id") Integer id) {
+        BookDto result = bookService.get(id);
         return ResponseEntity.ok(result);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") Integer id,
-                                    @RequestBody @Valid BookDto bookDto){
-        boolean result=bookService.update(id, bookDto);
+                                    @RequestBody @Valid BookDto bookDto) {
+        boolean result = bookService.update(id, bookDto);
         return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") Integer id){
-        boolean result =bookService.delete(id);
+    public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
+        boolean result = bookService.delete(id);
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("pagenation")
+    @GetMapping("/pagenation")
     public ResponseEntity<?> getAll(@RequestParam("p") Integer page,
-                                    @RequestParam("s") Integer size){
-        List<BookDto>result =bookService.findAllByPage(page, size);
+                                    @RequestParam("s") Integer size) {
+        List<BookDto> result = bookService.findAllByPage(page, size);
         return ResponseEntity.ok(result);
     }
+
     @GetMapping("/filter")
-    public ResponseEntity<?>filter(@RequestBody BookFilter bookFilter){
-        List<BookDto>result=bookService.filter(bookFilter);
+    public ResponseEntity<?> filter(@RequestBody BookFilter bookFilter) {
+        List<BookDto> result = bookService.filter(bookFilter);
         return ResponseEntity.ok(result);
     }
 
