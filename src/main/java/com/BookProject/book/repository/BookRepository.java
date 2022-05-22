a0ec4f398bbd2fcbbea6e32b9bb8e16c32076cca
akhmedovbobur <105979546+akhmedovbobur@users.noreply.github.com>
2022-05-22 17:15:54 +0500

    update

--- src/main/java/com/BookProject/book/repository/BookRepository.java
+++ src/main/java/com/BookProject/book/repository/BookRepository.java
@@ -1,4 +1,7 @@
 package com.BookProject.book.repository;
 
-public interface BookRepository {
+import com.BookProject.book.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
 }
